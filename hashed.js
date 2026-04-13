8e81e331b5f03dc94352c0e9697b582c3d8368f2
rootzin-had <usedtologin007@gmail.com>
2026-04-13 15:09:23 +0530

    Delete hashed.js

--- hashed.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const password = "imroot@121"; // choose your password
-
-bcrypt.hash(password, 10).then(hash => {
-  console.log("Hashed password:");
-  console.log(hash);
-});
